0662433b1332d5fcf554e616360cc6308d440a71
Alex Devero <deveroalex@gmail.com>
2017-12-27 09:40:07 +0100

    Add source files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/App'
+
+// Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
+let root = document.createElement('div')
+
+root.id = 'root'
+document.body.appendChild(root)
+
+// Now we can render our application into it
+render(<App />, document.getElementById('root'))
