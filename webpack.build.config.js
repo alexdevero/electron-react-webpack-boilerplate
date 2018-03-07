393325fcb3580daee0d97da6d7c723296b3cc354
Alex Devero <deveroalex@gmail.com>
2018-03-07 09:10:48 +0100

    Clean up configs

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -4,20 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const BabiliPlugin = require('babili-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-// Config directories
-const SRC_DIR = path.resolve(__dirname, 'src')
-const OUTPUT_DIR = path.resolve(__dirname, 'dist')
-
 // Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
-const defaultInclude = [SRC_DIR]
+const defaultInclude = path.resolve(__dirname, 'src')
 
 module.exports = {
-  entry: SRC_DIR + '/index.js',
-  output: {
-    path: OUTPUT_DIR,
-    publicPath: './',
-    filename: 'bundle.js'
-  },
   module: {
     rules: [
       {
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,20 +3,10 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { spawn } = require('child_process')
 
-// Config directories
-const SRC_DIR = path.resolve(__dirname, 'src')
-const OUTPUT_DIR = path.resolve(__dirname, 'dist')
-
 // Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
-const defaultInclude = [SRC_DIR]
+const defaultInclude = path.resolve(__dirname, 'src')
 
 module.exports = {
-  entry: SRC_DIR + '/index.js',
-  output: {
-    path: OUTPUT_DIR,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
   module: {
     rules: [
       {
@@ -50,7 +40,7 @@ module.exports = {
   ],
   devtool: 'cheap-source-map',
   devServer: {
-    contentBase: OUTPUT_DIR,
+    contentBase: path.resolve(__dirname, 'dist'),
     stats: {
       colors: true,
       chunks: false,
