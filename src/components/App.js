e9e05c54043b55808ddc8796ed17bcf56ebedd67
Sam Wang <sam@galileomd.com>
2019-04-13 01:16:20 -0400

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,34 @@
 import '../assets/css/App.css'
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Layout, Radio, Button, Popconfirm } from 'antd';
+const { Header, Content, Footer } = Layout;
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    }
+  };
+  
+  onChangeStatus = (e) => {
+  };
+
   render() {
     return (
-      <div>
+      <Layout className='layout'>
+        <Content className='content'>
         <h1>Hello, Electron!</h1>
 
         <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
-        
+        </Content>
+        <Footer className='footer'>
         <p>
           We are using node {process.versions.node}, 
           Chrome {process.versions.chrome}, 
           and Electron {process.versions.electron}.   
-        </p>
-      </div>
+        </p></Footer>
+      </Layout>
     )
   }
 }
