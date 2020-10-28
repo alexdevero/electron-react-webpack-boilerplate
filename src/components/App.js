65aad7042020c43ded2b01fa33750eab3a29c066
Alex Devero <deveroalex@gmail.com>
2020-10-28 11:21:52 +0100

    Rewrite App class to function

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
+import React from 'react'
+
 import '../assets/css/App.css'
-import React, { Component } from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello, Electron!</h1>
+function App() {
+  return (
+    <div>
+      <h1>Hello, Electron!</h1>
 
-        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
-      </div>
-    )
-  }
+      <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+    </div>
+  )
 }
 
 export default App
